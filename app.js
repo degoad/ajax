a0b0fa9f85bd4ac8391b00a68601c6cf545519fa
dokota goad <degoad@lcmail.lcsc.edu>
2019-10-03 12:30:33 -0700

    hi

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $.getJSON("data.json", function(data) {
     console.log(data);
+    $('#name').text( data.name );
+    $('#email').text( data.email );
+
+    $.each( data.games_owned, function(i) {
+      var li = $('<li></li>');
+      $(li).text( data.games_owned[i].name );
+      $('#games_owned').append( li );
+    })
   });
 })
